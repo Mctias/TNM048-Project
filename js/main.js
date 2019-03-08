3685eba1b7805d7bdbc8ba0d503de586742066fe
Joakim Svensk <joakim.svensk@foran.se>
2019-03-08 14:45:50 +0100

    Restructure, add slider and data files

--- js/main.js
+++ js/main.js
@@ -1,5 +1,34 @@
-var map;
-d3.json("./sweden_counties.geojson", function (data) {
-    //Working with the map
-    map = new map(data);
-});
+var map = new Map();
+
+map.init();
+map.loadData();
+
+let min_year = 1580;
+let max_year = 2000;
+let step = 20;
+let steps = Math.floor((max_year-min_year)/step);
+console.log(steps);
+
+var sliderTime = d3
+    .sliderBottom()
+    .min(min_year)
+    .max(max_year)
+    .ticks(steps)
+    .tickFormat(d3.format(''))
+    .step(step)
+    .width(750)
+    .on('onchange', val => {
+        d3.select('p#value-time').text(val);
+        map.update(val);
+    });
+
+var gTime = d3
+    .select('div#slider-time')
+    .append('svg')
+    .attr('width', 800)
+    .attr('height', 100)
+    .append('g')
+    .attr('transform', 'translate(25,25)');
+
+gTime.call(sliderTime);
+d3.select('p#value-time').text(min_year);
--- js/map.js
+++ js/map.js
@@ -1,127 +1,137 @@
-function map(data){
-	var mymap = L.map('mapid').setView([62, 15], 5);
+function Map(){
+	this.map = L.map('mapid').setView([62, 15], 5);
+	this.cities_geom = null;
+	this.cities_inhabitants = null;
+	this.swedish_counties = null;
 
-	var geojson;
-
-	//var geoJsonData = new L.GeoJSON.AJAX("../sweden_counties.geojson");   
-	
-	L.tileLayer('https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.png?access_token={accessToken}', {
-	    attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, <a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery © <a href="https://www.mapbox.com/">Mapbox</a>',
-	    maxZoom: 18,
-	    id: 'mapbox.streets',
-	    accessToken: 'pk.eyJ1IjoibWN0aWFzIiwiYSI6ImNqc2V2ZDB4ajE2dTc0M282azR0eTVocW8ifQ.IQP4GS8uS3dB3J5i1PGKZA'
-	}).addTo(mymap);
-	
-	var svgLayer = L.svg();
-	svgLayer.addTo(mymap);
-
-	//Adding a info box that displays info when you hover over
-	var info = L.control();
-
-	info.onAdd = function(map){
-		this._div = L.DomUtil.create('div', 'info');
-		this.update();
-		return this._div;
-	};
-
-	info.update = function(props){
-		this._div.innerHTML = '<h4>Någon kul data</h4>' + (props ? '<b>' + props.name + '<b> <br />' + props.density + 'st' : 'Hovra över ett län');
-	};
-
-	info.addTo(mymap);
-	
-	
-	
-	
-	var svg = d3.select("#mapid").select("svg");
-	var g = svg.append("g");
-	
-	d3.json("circles.json", function(collection) {
-		/* Add a LatLng object to each item in the dataset */
-		collection.objects.forEach(function(d) {
-			d.latLng = new L.LatLng(d.circle.coordinates[0],
-									d.circle.coordinates[1]);
-		});
-	
-		var feature = g.selectAll("circle")
-			.data(collection.objects)
-			.enter().append("circle")
-			.style("stroke", "black")
-			.style("opacity", .6)
-			.style("fill", "red")
-			.attr("r", 10);
-		
-		function update() {
-		    feature.attr("transform",
-		    	function(d) {
-		   			var layerPoint = mymap.latLngToLayerPoint(d.latLng);
-		     		return "translate("+ layerPoint.x +","+ layerPoint.y +")";
-		        }
-		    )
-		}
-				
-		update();
-		mymap.on("moveend", update);
-		
-	});
-	
 	//Styling of the layover
-	function style(){
-		return{
+	this.style = function(){
+		return {
 			fillColor: '',
 			weight: 1,
 			opacity: 1,
 			color: 'black',
 			fillOpacity: 0
 		};
-		
 	}
-	properties:({
 
-	})
-
-	//Styling of the layover when you highlight it
-	function highlightFeature(e){
-		var layer = e.target;
-
-		layer.setStyle({
-			weight: 5,
-			color: '#666',
-			dashArray: '',
-			fillOpacity: 0.7
+	this.onEachFeature = function(feature, layer) {
+		layer.on({
+			mouseover: highlightFeature,
+			mouseout: resetHighlight,
+			click: zoomToFeature
 		});
+	}
 
-		if(!L.Browser.ie && !L.Browser.opera && !L.Browser.edge){
-			layer.bringToFront();
+	this.init = function() {
+		L.tileLayer('https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.png?access_token={accessToken}', {
+			attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, <a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery © <a href="https://www.mapbox.com/">Mapbox</a>',
+			maxZoom: 18,
+			id: 'mapbox.streets',
+			accessToken: 'pk.eyJ1IjoibWN0aWFzIiwiYSI6ImNqc2V2ZDB4ajE2dTc0M282azR0eTVocW8ifQ.IQP4GS8uS3dB3J5i1PGKZA'
+		}).addTo(this.map);
+
+		var svgLayer = L.svg();
+		svgLayer.addTo(this.map);
+
+		//Adding a info box that displays info when you hover over
+		var info = L.control();
+		info.onAdd = function(map){
+			this._div = L.DomUtil.create('div', 'info');
+			this.update();
+			return this._div;
+		};
+		info.update = function(props){
+			this._div.innerHTML = '<h4>Någon kul data</h4>' + (props ? '<b>' + props.name + '<b> <br />' + props.density + 'st' : 'Hovra över ett län');
+		};
+		info.addTo(this.map);
+
+		var svg = d3.select("#mapid").select("svg");
+		var g = svg.append("g");
+		
+		d3.json("circles.json", function(collection) {
+			/* Add a LatLng object to each item in the dataset */
+			collection.objects.forEach(function(d) {
+				d.latLng = new L.LatLng(d.circle.coordinates[0],
+										d.circle.coordinates[1]);
+			});
+		
+			var feature = g.selectAll("circle")
+				.data(collection.objects)
+				.enter().append("circle")
+				.style("stroke", "black")
+				.style("opacity", .6)
+				.style("fill", "red")
+				.attr("r", 10);
+			
+			function update() {
+				feature.attr("transform",
+					function(d) {
+						var layerPoint = Map.latLngToLayerPoint(d.latLng);
+						return "translate("+ layerPoint.x +","+ layerPoint.y +")";
+					}
+				)
+			}	
+			update();
+			this.map.on("moveend", update);
+		});
+		
+		properties:({
+
+		})
+
+		//Styling of the layover when you highlight it
+		function highlightFeature(e){
+			var layer = e.target;
+
+			layer.setStyle({
+				weight: 5,
+				color: '#666',
+				dashArray: '',
+				fillOpacity: 0.7
+			});
+
+			if(!L.Browser.ie && !L.Browser.opera && !L.Browser.edge){
+				layer.bringToFront();
+			}
+
+			info.update({name: 'lol', density: 100});
 		}
 
-		info.update({name: 'lol', density: 100});
+		//Reset of the layover when you dehighlight it
+		function resetHighlight(e){
+			geojson.resetStyle(e.target);
+			info.update();
+		}
+
+		function zoomToFeature(e) {
+			mymap.fitBounds(e.target.getBounds());
+		}
+	}
+	
+	this.loadData = function() {
+		d3.json("./cities_geom.geojson", function (data) {
+			this.cities_geom = L.geoJson(data, {
+					style: this.style,
+					onEachFeature: this.onEachFeature
+				}).addTo(this.map);
+		});
+
+		d3.json("./sweden_counties.geojson", function (data) {
+			this.sweden_counties = L.geoJson(data, {
+				style: this.style,
+				onEachFeature: this.onEachFeature
+			}).addTo(this.map);
+		});
+
+		d3.json("./cities_inhabitants.json", function (data) {
+			
+		});
 	}
 
-	//Reset of the layover when you dehighlight it
-	function resetHighlight(e){
-		geojson.resetStyle(e.target);
-		info.update();
+	this.update = function(year) {
+		
 	}
-
-	function zoomToFeature(e) {
-    	mymap.fitBounds(e.target.getBounds());
-	}
-
-	//all the features
-	function onEachFeature(feature, layer) {
-   		layer.on({
-        	mouseover: highlightFeature,
-        	mouseout: resetHighlight,
-        	click: zoomToFeature
-    	});
-	}	
-
-	//Map the layover
-	geojson = L.geoJson(data, 
-			{style: style,
-			onEachFeature: onEachFeature}).addTo(mymap);
-
 }
 
 
